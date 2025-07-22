aae1ac93b05c138072ae9ca2b2d8e298ef235f5d
水島アキラ <YAMABIKO.yamazusi@gmail.com>
2025-07-22 23:26:02 +0900

    初期設定

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>初期画面</Text>
     </View>
   );
 }
